95f4a1c091b787c31d828030e6f9f9cfee8ed2c4
Alejandro M. Ravasio <alejandroravasio@gmail.com>
2025-12-29 19:02:35 +0000

    world: move grid size vars to their own constants

--- src/bootstrap/world.ts
+++ src/bootstrap/world.ts
@@ -1,7 +1,9 @@
 import { createWorld, TileFlag } from '../world';
 
 export function createInitialWorld() {
-  const worldSize = { width: 7, height: 7 };
+  const WIDTH = 7;
+  const HEIGHT = 7;
+  const worldSize = { width: WIDTH, height: HEIGHT };
   const world = createWorld(worldSize, 0, TileFlag.Walkable);
   const center = { x: Math.floor(worldSize.width / 2), y: Math.floor(worldSize.height / 2) };
   const bottomY = worldSize.height - 1;
